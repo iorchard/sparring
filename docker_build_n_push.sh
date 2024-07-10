65c5d03293b405e7f9f1c1a7041885811bac2016
Heechul Kim <jijisa@iorchard.co.kr>
2024-07-10 13:20:07 +0900

    update docker build and push script; change tag name

--- docker_build_n_push.sh
+++ docker_build_n_push.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Get robotframework version
-rf_version=$(python -m pip show robotframework  |grep Version: |cut -d':' -f2 |tr -d ' ')
+RF_VERSION=$(python -m pip show robotframework  |grep Version: |cut -d':' -f2 |tr -d ' ')
+# Get sparring version
+SPARRING_VERSION=$(git describe --tags --abbrev=0 2>/dev/null)
 docker build -t jijisa/sparring .
-docker tag jijisa/sparring jijisa/sparring:${rf_version}-yoga
+docker tag jijisa/sparring jijisa/sparring:${SPARRING_VERSION}-${RF_VERSION}
 docker push jijisa/sparring
-docker push jijisa/sparring:${rf_version}-yoga
+docker push jijisa/sparring:${SPARRING_VERSION}-${RF_VERSION}
